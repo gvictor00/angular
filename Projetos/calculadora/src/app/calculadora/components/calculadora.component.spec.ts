8bc533547933f7280cdbe0b3e5d127137ce04423
George <gvictor00@gmail.com>
2017-12-26 18:09:16 -0200

    Calculadora Finalizada

--- Projetos/calculadora/src/app/calculadora/components/calculadora.component.spec.ts
+++ Projetos/calculadora/src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
-import { CalculadoraService } from '../services'
+import { CalculadoraService } from '../services';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
--- Projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
+++ Projetos/calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -1,59 +1,57 @@
 /**
-*	Serviço responsável por executar as
-* operações da calculadora.
-*
-* @author George Alves
-* @since 1.0.0
-*/
+ * Serviço responsável por executar as operações da 
+ * calculadora.
+ * 
+ * @author Márcio C. de Souza<contato@kazale.com>
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CalculadoraService {
 
-	// readonly, somente leitura.
-	static readonly SOMA: string = '+';
-	static readonly SUBTRACAO: string = '-';
-	static readonly DIVISAO: string = '/';
-	static readonly MULTIPLICACAO: string = '*';
+  /* Define as constantes utilizadas 
+     para identificar as operações de cálculo */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
 
-	constructor() { }
+  constructor() { }
 
-	/**
-	*	Método para calculo da operação matemática
-	* com dois números. São suportadas as quatro
-	* operações básicas.
-	*
-	* @param num1 number
-	* @param num2 number
-	* @param operacao string Operação a ser executada
-	* @return number Resultado da operação
-	*/
-	calcular(num1: number, num2: number, operacao: string): number{
-		let resultado : number; // armazena o resultado
+  /**
+   * Método que calcula uma operação matemática dado 
+   * dois números.
+   * Suporta as operações soma, subtração, divisão, 
+   * e multiplicação.
+   *
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @return number Resultado da operação
+   */
+  calcular(num1: number, num2: number, operacao: string): number {
+  	let resultado: number; // armazena o resultado da operação
 
-		switch (operacao) {
-			case CalculadoraService.SOMA:
-			resultado = num1 + num2;
-			break;
-			case CalculadoraService.SUBTRACAO:
-			resultado = num1 - num2;
-			break;
-			case CalculadoraService.DIVISAO:
-			if(num2 == 0){
-				resultado = 0;
-			}else{
-				resultado = num1/num2;
-			}
-			break;
-			case CalculadoraService.MULTIPLICACAO:
-			resultado = num1*num2;
-			break;
-			
-			default:
-			resultado = 0;
-			break;
-		}
-		return resultado;
-	}
+  	switch(operacao) {
+  	  case CalculadoraService.SOMA:
+  	    resultado = num1 + num2;
+  		break;
+  	  case CalculadoraService.SUBTRACAO:
+  	    resultado = num1 - num2;
+  		break;
+  	  case CalculadoraService.DIVISAO:
+  	    resultado = num1 / num2;
+  		break;
+  	  case CalculadoraService.MULTIPLICACAO:
+  	    resultado = num1 * num2;
+  		break;
+  	  default:
+  	    resultado = 0;
+  	}
+
+  	return resultado;
+  }
 
 }
