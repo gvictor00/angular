19b357292c2fdfb1157eae5f65e0e819ceea125f
George <gvictor00@gmail.com>
2017-12-26 14:57:18 -0200

    Desenvolvido os components, configurados e realizados testes unitários

---
+++ Projetos/calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CalculadoraService } from '../services';
+@Component({
+	selector: 'app-calculadora',
+	templateUrl: './calculadora.component.html',
+	styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent implements OnInit {
+
+	// private variável : tipo
+	constructor(private calculadoraService: CalculadoraService) { }
+
+	ngOnInit() {
+		
+	}
+
+}
